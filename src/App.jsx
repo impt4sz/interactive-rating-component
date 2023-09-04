2b2abb973d21fd2f6fa0034c8cd95795c3a410c4
Harsha Vardhan <115931051+harsha-rvh@users.noreply.github.com>
2023-09-04 22:53:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import RatingForm from './RatingForm'
+import ThankYou from './ThankYou'
+import { useState } from 'react';
+
+function App() {
+  const [value, setValue] = useState(3);
+  let [isSubmitted, setIsSubmitted] = useState(false);
+  let content;
+
+  const changeValue = (event, newValue) => {
+    setValue(newValue);
+  }
+
+  const onClick = () => {
+    setIsSubmitted(!isSubmitted);
+    console.log(value);
+    console.log(isSubmitted);
+  }
+
+  if (isSubmitted) {
+    content = <ThankYou value={value} onClick={onClick} />
+  } else {
+    content = <RatingForm value={value} onClick={onClick} changeValue={changeValue} />
+  }
+
+  return (
+    <>
+      {content}
+    </>
+  )
+}
+
+export default App
---
+++ src/ThankYou.jsx
@@ -0,0 +1,80 @@
+import { Typography, Box, Button } from "@mui/material";
+
+export default function ThankYou({ value, onClick }) {
+    return (
+        <>
+            <Box
+                p={3}
+                sx={{
+                    maxWidth: 300,
+                    maxHeight: '100%',
+                    backgroundColor: '#252d37',
+                    borderRadius: '10%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Box
+                    component="img"
+                    src="/assets/illustration-thank-you.svg"
+                    alt="small icon of a reciept being printed"
+                    sx={{
+                        my: '1rem'
+                    }}
+                >
+                </Box>
+                <Typography
+                    sx={{
+                        color: 'var(--orange)',
+                        borderRadius: '20px',
+                        fontSize: 'var(--paragraph-15)',
+                        fontWeight: 400,
+                        my: '1rem'
+                    }}
+                >
+                    You selected {value} out of 5
+                </Typography>
+                <Typography
+                    component='h1'
+                    variant='h5'
+                    sx={{
+                        color: 'white'
+
+                    }}
+                >
+                    Thank you!
+                </Typography>
+                <Typography
+                    component='p'
+                    sx={{
+                        color: 'var(--light-grey)',
+                        fontSize: 'var(--paragraph-15)',
+                        fontWeight: 400,
+                        my: '1rem',
+                        textAlign: 'center'
+                    }}>
+                    We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!
+                </Typography>
+                <Button
+                    variant="contained"
+                    onClick={onClick}
+                    sx={{
+                        backgroundColor: 'var(--orange)',
+                        width: '18rem',
+                        borderRadius: '20px',
+                        mt: '1rem',
+                        transition: "background 0.5s, color 0.5s",
+                        "&:hover": {
+                            backgroundColor: 'white',
+                            color: 'var(--orange)'
+                        }
+                    }}
+                >
+                    Change Rating
+                </Button>
+            </Box >
+        </>
+    )
+}
